a315c6849d378b582ce4dca4d85bee231b1b0f1e
Thierry CANTENOT <thierry.cantenot@insa-lyon.fr>
2015-05-18 12:35:13 +0800

    Added random functions

---
+++ final/assets/shaders/random.glsl
@@ -0,0 +1,248 @@
+float rand(vec2 n)
+{
+    return fract(sin(dot(n.xy, vec2(12.9898, 78.233))) * 43758.5453);
+}
+
+float hash(vec2 n)
+{
+    return fract(sin(dot(n, vec2(1.0, 113.0))) * 13.5453123);
+}
+
+vec2 hash2(vec2 n)
+{
+	float x = dot(n, vec2(1.0, 113.00));
+    return fract(sin(vec2(x, x+1.0)) * vec2(13.5453123, 31.1459123));
+}
+
+vec3 hash3(vec2 n)
+{
+	float x = dot(n, vec2(1.0, 113.00));
+    return fract(sin(vec3(x, x+1.0, x+2.0)) * vec3(13.5453123, 31.1459123, 37.3490423));
+}
+
+
+
+// Generate a random point on unit disk with probability density rho = 1/pi
+// using a polar mapping
+vec2 rDiskPolar(vec2 seed)
+{
+    const float TwoPi = 2.0 * 3.141592654;
+
+    vec2 h = hash2(seed);
+
+    float r   = sqrt(h.x);   // [0, 1]
+    float phi = TwoPi * h.y; // [0, 2pi]
+
+    vec2 p;
+    p.x = r * cos(phi);
+    p.y = r * sin(phi);
+
+    return p;
+}
+
+// Generate a random point on unit disk with probability density rho = 1/pi
+// using a concentric mapping
+vec2 rDiskPolar(vec2 seed)
+{
+    const float Pi = 3.141592654;
+
+    vec2 h = hash2(seed);
+    float r1 = h.x;
+    float r2 = h.y;
+
+    float r;   // [0, 1]
+    float phi; // [0, 2pi]
+
+    // First triangular region
+    if(r1 > -r2 && r1 > r2)
+    {
+        r = r1;
+        phi = (pi / 4.0) * (r2 / r1);
+    }
+    // Second triangular region
+    else if(r1 < r2 && r1 > -r2)
+    {
+        r = r2;
+        phi = (pi / 4.0) * (2.0 - r1 / r2);
+    }
+    // Third triangular region
+    else if(r1 < -r2 && r1 < r2)
+    {
+        r = -r1;
+        phi = (pi / 4.0) * (4.0 + r2 / r1);
+    }
+    // Fourth triangular region
+    else if(r1 > r2 && r1 < -r2)
+    {
+        r = -r2;
+        phi = (pi / 4.0) * (6.0 - r1 / r2);
+    }
+
+    vec2 p;
+    p.x = r * cos(phi);
+    p.y = r * sin(phi);
+
+    return p;
+}
+
+// Generate a random point on sphere ((0, 0, 0), r)
+// with probability density rho = 1 / (4 * pi * r^2)
+vec3 rSphere(float r, vec2 seed)
+{
+    const float TwoPi = 2.0 * 3.141592654;
+
+    vec2 h = hash2(seed);
+    float r1 = h.x;
+    float r2 = h.y;
+
+    float s = 2.0 * r * sqrt(r2 * (1.0 - r2));
+    float phi = TwoPi * r1;
+
+    vec3 p;
+    p.x = cos(phi) * s;
+    p.y = sin(phi) * s;
+    p.z = r * (1.0 - 2.0 * r2);
+
+    return p;
+}
+
+// Generate a random point on sphere (c, r)
+// with density rho = 1 / (4 * pi * r^2)
+vec3 rSphere(vec3 c, float r, vec2 seed)
+{
+    return c + rSphere(r, seed);
+}
+
+// Generate uniform random direction on unit hemisphere
+// with probability density rho = 1/2pi
+vec3 rHemisphereUniform(vec2 seed)
+{
+    const float TwoPi = 2.0 * 3.141592654;
+
+    vec2 h = hash2(seed);
+
+    float phi = TwoPi * h.x;
+    float cosTheta = h.y;
+    float sinTheta = sqrt(1.0 - cosTheta * cosTheta);
+
+    vec3 p;
+    p.x = cos(phi) * sinTheta;
+    p.y = sin(phi) * sinTheta;
+    p.z = cosTheta;
+
+    return p;
+}
+// Generate uniform random direction on unit hemisphere proportinal to solid angle
+// with probability density rho = 1/(2pi * (1 - cos(thetaMax))
+// theta: [0, thetaMax]
+vec3 rHemisphereUniform(float thetaMax, vec2 seed)
+{
+    const float TwoPi = 2.0 * 3.141592654;
+
+    vec2 h = hash2(seed);
+
+    float cosThetaMax = cos(thetaMax);
+
+    float phi = TwoPi * h.x;
+    float cosTheta = 1.0 - h.y * (1.0 - cosThetaMax);
+    float sinTheta = sqrt(1.0 - cosTheta * cosTheta);
+
+    vec3 p;
+    p.x = cos(phi) * sinTheta;
+    p.y = sin(phi) * sinTheta;
+    p.z = cosTheta;
+
+    return p;
+}
+
+// Generate random direction on unit hemisphere proportinal to cosine-weighted solid angle
+// with probability density rho = cos(theta) / pi
+// theta: [0, pi]
+vec3 rHemisphereCosine(vec2 seed)
+{
+    const float TwoPi = 2.0 * 3.141592654;
+
+    vec2 h = hash2(seed);
+
+    float phi = TwoPi * h.x;
+    float s = sqrt(1.0 - h.y)
+
+    vec3 p;
+    p.x = cos(phi) * s;
+    p.y = sin(phi) * s;
+    p.z = sqrt(h.y);
+
+    return p;
+}
+
+// Generate random direction on unit hemisphere proportinal to cosine-weighted solid angle
+// with probability density rho = cos(theta) / (pi * (sin(thetaMax))^2)
+// theta: [0, thetaMax]
+vec3 rHemisphereCosine(float thetaMax, vec2 seed)
+{
+    const float TwoPi = 2.0 * 3.141592654;
+
+    vec2 h = hash2(seed);
+
+    float sinThetaMax = sin(thetaMax);
+
+    float phi = TwoPi * h.x;
+    float s = sinThetaMax * sqrt(h.y)
+
+    vec3 p;
+    p.x = cos(phi) * s;
+    p.y = sin(phi) * s;
+    p.z = sqrt(1.0 - h.y * sinThetaMax * sinThetaMax);
+
+    return p;
+}
+
+// Generate random direction on unit hemisphere proportinal to cosine lobe around normal
+// with probability density rho = ((n+1)/2pi) * (cos(theta))^n
+// theta: [0, pi]
+// => General formula:
+//  n = 0 -> rHemisphereUniform
+//  n = 1 -> rHemisphereCosine
+vec3 rHemisphereCosineGeneral(float n, vec2 seed)
+{
+    const float TwoPi = 2.0 * 3.141592654;
+
+    vec2 h = hash2(seed);
+
+    float phi = TwoPi * h.x;
+    float cosTheta = pow(h.y, 1.0 / (n + 1.0));
+    float sinTheta = sqrt(1.0 - cosTheta * cosTheta);
+
+    vec3 p;
+    p.x = cos(phi) * sinTheta;
+    p.y = sin(phi) * sinTheta;
+    p.z = cosTheta;
+
+    return p;
+}
+
+// Generate random direction on unit hemisphere proportinal to cosine lobe around normal
+// with probability density rho = ((n+1)/2pi) * (cos(theta))^n/(1-(cos(thetaMax))^(n+1))
+// theta: [0, thetaMax]
+// => General formula:
+//  n = 0 -> rHemisphereUniform
+//  n = 1 -> rHemisphereCosine
+vec3 rHemisphereCosineGeneral(float thetaMax, float n, vec2 seed)
+{
+    const float TwoPi = 2.0 * 3.141592654;
+
+    vec2 h = hash2(seed);
+
+    float cosThetaMax = cos(thetaMax);
+
+    float phi = TwoPi * h.x;
+    float cosTheta = pow(1.0 - h.y * (1.0 - pow(cosThetaMax, n + 1.0)), 1.0 / (n + 1.0));
+    float sinTheta = sqrt(1.0 - cosTheta * cosTheta);
+
+    vec3 p;
+    p.x = cos(phi) * sinTheta;
+    p.y = sin(phi) * sinTheta;
+    p.z = cosTheta;
+
+    return p;
+}
